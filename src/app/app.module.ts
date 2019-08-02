970a5005f2e71bdcf1176a53213b56950b2b18ff
amitrega01 <amitrega01@gmail.com>
2019-08-02 13:39:37 +0200

    bootstrap and form reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,29 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { AngularFireModule } from "@angular/fire";
-import { environment } from "src/environments/environment";
-import { MatMenuModule } from "@angular/material/menu";
-import { FormsModule } from "@angular/forms";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { CompetitionsComponent } from "./competitions/competitions.component";
-import { CompetitionComponent } from "./competition/competition.component";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTableModule } from "@angular/material/table";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { CompetitionsComponent } from './competitions/competitions.component';
+import { CompetitionComponent } from './competition/competition.component';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [AppComponent, CompetitionsComponent, CompetitionComponent],
   imports: [
+    NgbModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatTableModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
