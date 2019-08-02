5bd1aa6e42bf6d6f6bb0c0a717b0aac7587c67ae
amitrega01 <amitrega01@gmail.com>
2019-08-02 11:57:12 +0200

    refacoring

--- src/app/competitions/competitions.component.ts
+++ src/app/competitions/competitions.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Observable } from "rxjs";
-import {
-  AngularFirestore,
-  AngularFirestoreDocument,
-  AngularFirestoreCollection
-} from "@angular/fire/firestore";
-import { Competition } from "../interfaces/Competition";
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Competition } from '../interfaces/Competition';
 
 @Component({
-  selector: "app-competitions",
-  templateUrl: "./competitions.component.html"
+  selector: 'app-competitions',
+  templateUrl: './competitions.component.html'
 })
 export class CompetitionsComponent {
   private itemsDoc: AngularFirestoreCollection<Competition>;
   items: Observable<Competition[]>;
   currentCompetition: any;
   constructor(private afs: AngularFirestore) {
-    this.itemsDoc = afs.collection("competitions");
+    this.itemsDoc = afs.collection('competitions');
     this.items = this.itemsDoc.valueChanges();
   }
   changeCompetition(item) {
