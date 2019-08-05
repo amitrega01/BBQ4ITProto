f22fb1963cdade06fb5489353ceb28e059306222
Cholewa98 <michal.cholewka98@interia.pl>
2019-08-05 13:27:23 +0200

    login alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/auth';
 
+
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="navbar">
-      BBQ4.IT
-      <button class="logoutButton" *ngIf="afAuth.user | async as user" (click)="onClickLogout()">
-        <i class="fa fa-sign-out" style="font-size:20px;color:white"></i>
-        Logout
-      </button>
-    </div>
-
-    <div *ngIf="afAuth.user | async as user; else showLogin">
-      <app-competitions></app-competitions>
-    </div>
-    <ng-template #showLogin>
-      <div class="loginPanel">
-        <form class="loginForm" #userlogin="ngForm" (ngSubmit)="onClickSubmit(userlogin.value)">
-          <h2>Logowanie</h2>
-          <br />
-          <input class="loginInput" type="text" name="email" placeholder="Email" ngModel />
-
-          <input class="loginInput" type="password" name="password" placeholder="Password" ngModel />
-
-          <input class="loginInput" type="submit" value="Zaloguj" />
-        </form>
-      </div>
-    </ng-template>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   email: string;
   password: string;
-
+  alertVisible: boolean=false;
+  alertContent: string;
   constructor(public afAuth: AngularFireAuth) {}
 
   onClickSubmit(data) {
@@ -44,7 +22,8 @@ export class AppComponent {
         console.log(res);
       })
       .catch(error => {
-        alert(error.message);
+        this.alertContent=error.message;
+        this.alertVisible=true;
       });
   }
   onClickLogout() {
@@ -55,4 +34,10 @@ export class AppComponent {
   logout() {
     this.afAuth.auth.signOut();
   }
+  closeAlert() {
+    this.alertVisible=false;
+  }
+ 
 }
+
+
