6e4a1664441b995bb026d7494d247ac9473233b1
PiotrekG_ <48363758+PiotrekkG@users.noreply.github.com>
2019-08-05 11:20:25 +0200

    logout upgrade, competition list upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,14 @@ import { AngularFireAuth } from '@angular/fire/auth';
   selector: 'app-root',
   template: `
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" routerLink="#">BBQ4.IT</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <button  *ngIf="afAuth.user | async as user" (click)="onClickLogout()" class="nav-item nav-link active logoutButton" >Logout <span class="sr-only">(current)</span></button>
-    </div>
+  <div class= "navbar">
+    BBQ4.IT
+    <button class="logoutButton" *ngIf="afAuth.user | async as user" (click)="onClickLogout()"> 
+    <i class="fa fa-sign-out" style="font-size:20px;color:white"></i>
+      Logout
+    </button>
   </div>
-  </nav>
-  
+
   <div class="xd">
   
   
