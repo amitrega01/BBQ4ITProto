e15d58ee39f3eef2624d7fd18942dc79b1327bfe
amitrega01 <amitrega01@gmail.com>
2019-08-05 09:41:20 +0200

    reject alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,45 +5,53 @@ import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-root',
   template: `
-
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" routerLink="#">BBQ4.IT</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <button  *ngIf="afAuth.user | async as user" (click)="onClickLogout()" class="nav-item nav-link active logoutButton" >Logout <span class="sr-only">(current)</span></button>
-    </div>
-  </div>
-  </nav>
-  
-  <div class="xd">
-  
-  
-    <div *ngIf="afAuth.user | async as user; else showLogin">
-      <app-competitions></app-competitions>
-    </div>
- 
-    <div class="loginPanel"> 
-   
-    <ng-template #showLogin>
-     
-      <div id="test">
-        <form  #userlogin="ngForm" (ngSubmit)="onClickSubmit(userlogin.value)">
-          <h2>Login panel</h2>
-          <br/>
-          <input type="text" name="email" placeholder="Email" ngModel />
-          <br />
-          <input type="password" name="password" placeholder="Password" ngModel />
-          <br />
-          <input type="submit" value="submit" />
-        </form>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" routerLink="#">BBQ4.IT</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <button
+            *ngIf="afAuth.user | async as user"
+            (click)="onClickLogout()"
+            class="nav-item nav-link active logoutButton"
+          >
+            Logout <span class="sr-only">(current)</span>
+          </button>
+        </div>
       </div>
-    </ng-template>
+    </nav>
+
+    <div class="xd">
+      <div *ngIf="afAuth.user | async as user; else showLogin">
+        <app-competitions></app-competitions>
+      </div>
+
+      <div class="loginPanel">
+        <ng-template #showLogin>
+          <div id="test">
+            <form #userlogin="ngForm" (ngSubmit)="onClickSubmit(userlogin.value)">
+              <h2>Login panel</h2>
+              <br />
+              <input type="text" name="email" placeholder="Email" ngModel />
+              <br />
+              <input type="password" name="password" placeholder="Password" ngModel />
+              <br />
+              <input type="submit" value="submit" />
+            </form>
+          </div>
+        </ng-template>
+      </div>
     </div>
-    </div>
-    
   `
 })
 export class AppComponent {
@@ -55,16 +63,19 @@ export class AppComponent {
     console.log('Init');
   }
   onClickSubmit(data) {
-    this.afAuth.auth.signInWithEmailAndPassword(data.email, data.password).then(res => {
-      console.log(res);
-    });
+    this.afAuth.auth
+      .signInWithEmailAndPassword(data.email, data.password)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        alert(error.message);
+      });
   }
-  onClickLogout()
-  {
+  onClickLogout() {
     this.afAuth.auth.signOut();
   }
 
-  
   login() {}
   logout() {
     this.afAuth.auth.signOut();
