1e10b1c692b4c4fcf15af3052c9786e9a70fd442
Adam Mitręga <amitrega01@gmail.com>
2019-08-07 09:18:53 +0200

    firebase functions init

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
