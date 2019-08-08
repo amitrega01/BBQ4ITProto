590a3100a530e2e65352e3b8e3b0b2fc66d15052
Adam Mitręga <amitrega01@gmail.com>
2019-08-08 11:30:38 +0200

    post api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const app = express();
+app.use(express.json());
+
+//https://us-central1-bbq4it-b4163.cloudfunctions.net/api/newScore/0oz0p3Ce11M8l3RMwLzM
+app.post('/newScore/:route', (req, res) => {
+  db.collection(req.params.route)
+    .add(req.body)
+    .then((msg: any) => {
+      console.log(msg);
+      res.send(msg);
+    })
+    .catch((error: any) => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
