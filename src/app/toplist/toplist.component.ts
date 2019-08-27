5c57bc0c5ef1b277095b0b7f2b26593a0af5e839
Cholewa98 <michal.cholewka98@interia.pl>
2019-08-27 13:05:12 +0200

    carousel auto spin fix

--- src/app/toplist/toplist.component.ts
+++ src/app/toplist/toplist.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, OnInit, EventEmitter } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Competition } from '../interfaces/Competition';
-import { Observable } from 'rxjs';
+import { Observable, interval } from 'rxjs';
 import { Score } from '../interfaces/Score';
 import { CarouselComponent } from 'ngx-carousel-lib';
 import { delay } from 'q';
@@ -24,61 +24,77 @@ export class TopListComponent implements OnInit {
   currentItem: Score;
   mobile: Boolean;
   carouselSize: number;
+  interval: any;
   changeCompetition(item) {
     console.log(item);
     this.currentCompetition = item;
   }
-  ngOnInit() { 
-  
+  ngOnInit() {
+
     if (screen.width < 576) { // 768px portrait
       this.mobile = true;
     }
-    else this.mobile= false;
+    else this.mobile = false;
     // this.topCarousel.loop = true;
     // this.topCarousel.autoPlay = true;
     // this.topCarousel.delayAutoPlay = 1000;
     // this.topCarousel.autoPlayStart()
-    
+
   }
 
 
   constructor(afs: AngularFirestore) {
     this.itemsDoc = afs.collection('competitions');
     this.items = this.itemsDoc.valueChanges();
-    
+
   }
-  @ViewChild('topCarousel' ,{static: false,}) topCarousel: CarouselComponent;
-  
+  @ViewChild('topCarousel', { static: false, }) topCarousel: CarouselComponent;
+
   prev() {
     // this.topCarousel.loop = true;
     // this.topCarousel.autoPlay = true;
     // this.topCarousel.delayAutoPlay = 1000;
     // this.topCarousel.autoPlayStart()
-    this.carouselSize=this.topCarousel.carousel.totalItems;
+    this.carouselSize = this.topCarousel.carousel.totalItems;
     console.log(this.carouselSize);
-  
-    if(this.topCarousel.carousel.activeIndex==0) {
-      this.topCarousel.slideTo(this.carouselSize-1);
+
+    if (this.topCarousel.carousel.activeIndex == 0) {
+      this.topCarousel.slideTo(this.carouselSize - 1);
     }
     else this.topCarousel.slidePrev();
-    
+
   }
 
   next() {
-   
-    if(this.topCarousel.carousel.activeIndex+1==this.topCarousel.carousel.totalItems) {
-        this.topCarousel.slideTo(0);
+
+    if (this.topCarousel.carousel.activeIndex + 1 == this.topCarousel.carousel.totalItems) {
+      this.topCarousel.slideTo(0);
     }
-    else  this.topCarousel.slideNext();
-}
+    else this.topCarousel.slideNext();
+  }
   start() {
-    console.log('dziala');
-    var interval = setInterval(()=>{
-      if(this.topCarousel.carousel.activeIndex+1==this.topCarousel.carousel.totalItems) {
-        this.topCarousel.slideTo(0);
+  this.interval = setInterval(() => {
+    if (this.topCarousel.carousel.activeIndex + 1 == this.topCarousel.carousel.totalItems) {
+      this.topCarousel.slideTo(0);
     }
-    else  this.topCarousel.slideNext();
-    },8000);
+    else this.topCarousel.slideNext();
+  }, 1000);
+  }
+  stop() {
+    console.log('dziala');
+    if (this.interval == undefined) {
+
+      this.interval = setInterval(() => {
+        if (this.topCarousel.carousel.activeIndex + 1 == this.topCarousel.carousel.totalItems) {
+          this.topCarousel.slideTo(0);
+        }
+        else this.topCarousel.slideNext();
+      }, 5000);
+    } else {
+      console.log(typeof (this.interval))
+      clearInterval(this.interval)
+      this.interval = undefined;
+    }
   }
 
   getScreenWidth(event) {
@@ -87,6 +103,6 @@ export class TopListComponent implements OnInit {
     if (w < 576) { // 768px portrait
       this.mobile = true;
     }
-    else this.mobile= false;
+    else this.mobile = false;
   }
 }
